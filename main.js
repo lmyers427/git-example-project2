43d5111811ff312f08174f1cf367a4927f1d3b03
Lauren Myers <lmyers@elkgrovecity.org>
2021-11-20 18:57:54 -0800

    First Commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let  myValue = "";
+let myString = "coconut";
+
+  /////////// create your loop below this line ///////////
+  for(let i = myString.length - 1; i >= 0; i--)
+  {
+    
+      
+    myValue += myString.charAt(i);
+   
+  
+  }
+  
+  
+    /////////// create your loop above this line ///////////
+    return myValue;
+console.log(myValue);
+  
